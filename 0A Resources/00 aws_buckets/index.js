2bf30bdb742e41b58b8ef593b8978769de44bd01
JaimeSalas <jaime.salas@lemoncode.net>
2017-06-17 17:50:52 +0200

    basic example working

---
+++ 0A Resources/00 aws_buckets/index.js
@@ -0,0 +1,11 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+s3.listBuckets((err, data) => {
+    if (err) console.log('Error', err);
+    console.log('Bucket list', data.Buckets);
+});
+
+// console.log('Heloo');
